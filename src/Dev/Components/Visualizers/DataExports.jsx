d0ef168a4ddcd9204fc542f1db50dab71b4502ff
adedolapoadegboye <adedolapo.adegboye@alumni.ucalgary.ca>
2024-08-01 04:12:06 -0600

    export data completed

--- src/Dev/Components/Visualizers/DataExports.jsx
+++ src/Dev/Components/Visualizers/DataExports.jsx
@@ -27,7 +27,7 @@ const DataExports = (props) => {
     altPlotData,
   } = props;
 
-  const exportToExcel = () => {
+  const exportToExcelAndKml = () => {
     // Data for the second sheet
     const fileData = file.data.map((item, index) => {
       const dataEntry = {
@@ -77,7 +77,7 @@ const DataExports = (props) => {
 
     // Create the second sheet and append it to the workbook
     const ws2 = XLSX.utils.json_to_sheet(fileData);
-    XLSX.utils.book_append_sheet(workbook, ws2, "File Data");
+    XLSX.utils.book_append_sheet(workbook, ws2, "Data");
 
     // Write the workbook to a binary string
     const excelBuffer = XLSX.write(workbook, {
@@ -86,7 +86,9 @@ const DataExports = (props) => {
     });
 
     // Create a Blob object from the binary string
-    const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+    const excelBlob = new Blob([excelBuffer], {
+      type: "application/octet-stream",
+    });
 
     // Generate a timestamp for the filename
     const timestamp = new Date()
@@ -94,15 +96,45 @@ const DataExports = (props) => {
       .replace(/[-:T.]/g, "")
       .slice(0, 14);
 
-    // Trigger the file download
-    saveAs(blob, `data_results_${timestamp}.xlsx`);
+    // Trigger the file download for the Excel file
+    saveAs(excelBlob, `data_results_${timestamp}.xlsx`);
+
+    // Generate KML content
+    const kmlContent = `<?xml version="1.0" encoding="UTF-8"?>
+      <kml xmlns="http://www.opengis.net/kml/2.2">
+        <Document>
+          <name>Position Fixes</name>
+          <description>Position Fixes from GNSS Data</description>
+          ${fileData
+            .map(
+              (item, index) => `
+          <Placemark>
+            <name>Fix ${index}</name>
+            <Point>
+              <coordinates>${item.Longitude},${item.Latitude},${
+                item.Altitude ? item.Altitude : 0
+              }</coordinates>
+            </Point>
+          </Placemark>`
+            )
+            .join("")}
+        </Document>
+      </kml>`;
+
+    // Create a Blob object for the KML file
+    const kmlBlob = new Blob([kmlContent], {
+      type: "application/vnd.google-earth.kml+xml",
+    });
+
+    // Trigger the file download for the KML file
+    saveAs(kmlBlob, `data_results_${timestamp}.kml`);
   };
 
   return (
     <div>
       <button
         className="h-fit w-fit px-6 py-3 border bg-gray-700 rounded-2xl text-white transition duration-300 ease-in-out hover:bg-blue-500"
-        onClick={exportToExcel}
+        onClick={exportToExcelAndKml}
       >
         Download Results
       </button>
